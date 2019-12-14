45f80bdb6547d7b09114f5825bffbd8440e00238
Ollanta CG <ollantaster@gmail.com>
2019-12-14 07:38:36 +0100

    Day 14: I should get better at typing

---
+++ day14/fuel.hs
@@ -0,0 +1,73 @@
+import Text.Parsec
+import Data.List as L
+import qualified Data.HashMap.Strict as M
+
+
+main :: IO ()
+main = do
+  s <- getContents
+  p <- either (error.show) return (readD s)
+  putStrLn (showD $ solve p)
+
+
+type Dependency = ([(Integer, String)], (Integer, String))
+
+type Req = (Integer, String)
+
+readD :: String -> Either ParseError [Dependency]
+readD s = parse (readReq `endBy` newline) "" s
+  where
+    readReq = do
+      prereqs <- (read1req `sepBy` string ", ")
+      spaces
+      string "=>"
+      spaces
+      result <- read1req
+      return (prereqs, result)
+    read1req = do
+      n <- many digit
+      space
+      s <- many alphaNum
+      return (read n :: Integer, s)
+
+
+showD = show
+
+
+solve :: [Dependency] -> Integer
+solve deps = cost
+  where
+    (_, cost) = costOf depmap 1 "FUEL" M.empty
+    depmap = M.fromList $ map (\(req,(n, t)) -> (t,(n,req))) deps
+
+costOf :: M.HashMap String (Integer, [(Integer,String)]) -> Integer -> String -> M.HashMap String Integer -> (M.HashMap String Integer, Integer)
+costOf depmap n "ORE" m = (m, fromInteger n)
+costOf depmap n s m     = (m''', cost)
+  where
+    (dn, reqs) = depmap M.! s
+
+    multiplier = ceiling (fromInteger n/ fromInteger dn)
+    rest       = multiplier*dn - n
+
+    mreqs = map (\(n,t) -> (multiplier*n, t)) reqs
+    (m', reducedreqs) = reducereq [] m mreqs
+
+    (m'', cost) = includereq depmap 0 m' reducedreqs
+
+    m''' = M.insert s rest m''
+
+includereq :: M.HashMap String (Integer, [(Integer,String)]) -> Integer -> M.HashMap String Integer -> [Req] -> (M.HashMap String Integer, Integer)
+includereq depmap acc m [] = (m, acc)
+includereq depmap acc m (req@(n,t):rs) = includereq depmap (acc+cost) m' rs
+  where
+    (m', cost) = costOf depmap n t m
+
+
+reducereq :: [Req] -> M.HashMap String Integer -> [Req] -> (M.HashMap String Integer, [Req])
+reducereq acc m [] = (m,acc)
+reducereq acc m (req@(n,t):rs)
+  | have == 0 = reducereq (req:acc) m rs
+  | have >= n = reducereq acc (M.insert t (have-n) m) rs
+  | otherwise = reducereq ((n-have, t):acc) (M.insert t 0 m) rs
+  where
+    have = M.lookupDefault 0 t m
---
+++ day14/fuel2.hs
@@ -0,0 +1,90 @@
+import Text.Parsec
+import Data.List as L
+import qualified Data.HashMap.Strict as M
+
+
+main :: IO ()
+main = do
+  s <- getContents
+  p <- either (error.show) return (readD s)
+  putStrLn (showD $ solve p)
+
+
+type Dependency = ([(Integer, String)], (Integer, String))
+
+type Req = (Integer, String)
+
+readD :: String -> Either ParseError [Dependency]
+readD s = parse (readReq `endBy` newline) "" s
+  where
+    readReq = do
+      prereqs <- (read1req `sepBy` string ", ")
+      spaces
+      string "=>"
+      spaces
+      result <- read1req
+      return (prereqs, result)
+    read1req = do
+      n <- many digit
+      space
+      s <- many alphaNum
+      return (read n :: Integer, s)
+
+
+showD = unlines . map show
+
+takeevery _ [] = []
+takeevery n l = p1 ++ takeevery n p2
+  where
+    (p1,p2) = helper n l
+    helper 1 (l:ls) = ([l],ls)
+    helper n (l:ls@(a:b:_)) = helper (n-1) ls
+    helper _ ls = (ls,[])
+
+
+solve :: [Dependency] -> [(Integer, Integer)]
+solve deps = search 0 1000000000000
+  where
+    maxCost = 1000000000000
+
+    search min max
+      | min == max-1 = [(min,max)]
+      | calcCost mid > maxCost = (min,max):search min mid
+      | otherwise = (min,max):search mid max
+      where
+        mid = (min + max) `div` 2
+
+    calcCost n = snd $ costOf depmap n "FUEL" M.empty
+    depmap = M.fromList $ map (\(req,(n, t)) -> (t,(n,req))) deps
+
+costOf :: M.HashMap String (Integer, [(Integer,String)]) -> Integer -> String -> M.HashMap String Integer -> (M.HashMap String Integer, Integer)
+costOf depmap n "ORE" m = (m, fromInteger n)
+costOf depmap n s m     = (m''', cost)
+  where
+    (dn, reqs) = depmap M.! s
+
+    multiplier = ceiling (fromInteger n/ fromInteger dn)
+    rest       = multiplier*dn - n
+
+    mreqs = map (\(n,t) -> (multiplier*n, t)) reqs
+    (m', reducedreqs) = reducereq [] m mreqs
+
+    (m'', cost) = includereq depmap 0 m' reducedreqs
+
+    m''' = M.insert s rest m''
+
+includereq :: M.HashMap String (Integer, [(Integer,String)]) -> Integer -> M.HashMap String Integer -> [Req] -> (M.HashMap String Integer, Integer)
+includereq depmap acc m [] = (m, acc)
+includereq depmap acc m (req@(n,t):rs) = includereq depmap (acc+cost) m' rs
+  where
+    (m', cost) = costOf depmap n t m
+
+
+reducereq :: [Req] -> M.HashMap String Integer -> [Req] -> (M.HashMap String Integer, [Req])
+reducereq acc m [] = (m,acc)
+reducereq acc m (req@(n,t):rs)
+  | have == 0 = reducereq (req:acc) m rs
+  | have >= n = reducereq acc (M.insert t (have-n) m) rs
+  | otherwise = reducereq ((n-have, t):acc) (M.insert t 0 m) rs
+  where
+    have = M.lookupDefault 0 t m

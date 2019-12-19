e4e1eae41ddc039a2f8b0f61981c7eac9909006e
Ollanta CG <ollantaster@gmail.com>
2019-12-19 22:08:38 +0100

    Day 19: cleanup

--- day19/tractor.hs
+++ day19/tractor.hs
@@ -1,16 +1,11 @@
-import Text.Parsec
-import qualified Data.Vector.Unboxed as V
 import qualified Data.HashMap.Strict as M
 import Helpers
 import Intcode
-import Data.Char
-
 
 main :: IO ()
 main = do
   s <- getContents
   putStrLn (showD . solve . readProgram $ s)
-
 
 showD (m, sol) = drawMap toVis m ++ show sol
 
@@ -20,11 +15,7 @@ toVis 0 = '.'
 solve :: Program -> (M.HashMap (Integer,Integer) Integer, Int)
 solve program = (chmap, M.size $ M.filter(==1) chmap)
   where
-    minX = 0
-    minY = 0
-    maxX = minX+49
-    maxY = minY+49
-    coords = [(x,y) | x <- [minX..maxX], y <- [minY..maxY]]
+    coords = [(x,y) | x <- [0..49], y <- [0..49]]
     outs = concat [run $ initComputer program [x,y] | (x,y) <- coords]
     chlist = zip coords outs
 
--- day19/tractor2.hs
+++ day19/tractor2.hs
@@ -1,33 +1,19 @@
-import Text.Parsec
-import qualified Data.Vector.Unboxed as V
-import qualified Data.HashMap.Strict as M
-import Helpers
 import Intcode
-import Data.Char
-
 
 main :: IO ()
 main = do
   s <- getContents
   putStrLn (showD . solve . readProgram $ s)
 
-
---showD (m, sol) = drawMap toVis m ++ show sol
 showD = show
 
-toVis 1 = '#'
-toVis 0 = '.'
-
---solve :: Program -> (M.HashMap (Integer,Integer) Integer, Int)
-solve program = result
+solve :: Program -> (Integer, Integer)
+solve program = loop (10,5) 99
   where
     -- seed the loop with a value on or above the (full) tractor beam
-    result = loop (100,60) 99
-
     loop c@(x,y) d
-      | not (check c)  = loop (x,y+1) d
-      | not (check (x,y+d)) = loop (x+1,y) d
-      | not (check (x+d,y)) = loop (x,y+1) d
+      | check (x,y+d) = loop (x+1,y) d
+      | check (x+d,y) = loop (x,y+1) d
       | otherwise = c
 
-    check (x,y) = (==1) . head . run $ initComputer program [x,y]
+    check (x,y) = (==0) . head . run $ initComputer program [x,y]
